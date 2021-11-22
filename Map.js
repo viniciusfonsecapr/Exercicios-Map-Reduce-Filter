58213054e0967ba0909f7489cc556f0e18038e4e
Vinicius Gabriel Fonseca <87347314+viniciusfonsecapr@users.noreply.github.com>
2021-11-22 00:18:23 -0300

    Add files via upload

---
+++ Map.js
@@ -0,0 +1,33 @@
+/* 
+
+    MAP 
+        -  Cria um novo array, a partir  do array percorrido (array original)
+        -  Cria um novo array, com a mesma quantida de itens do array original
+        - Aceita 3 parâmetros
+            - Item do array
+            - index
+            - Array compeleto
+*/
+
+const numbers = [1, 2, 3, 4]
+const students = [
+    { name: "Rodolfo", age: 25 },
+    { name: "Maria", age: 43 },
+    { name: "Joao", age: 21 },
+    { name: "Bruno", age: 43 },
+    { name: "Carla", age: 13 },
+    { name: "Ana", age: 20 },
+    { name: "Julio", age: 26 },
+];
+
+// const newArray = numbers.map((student)) => {
+//     return number * 2 
+// }
+
+// const newStudents = students.map ( (student) => { 
+//     const newStudent = {
+//         name: student.name + 'da Silva'
+//         age: student.age -5
+//     }
+//     return newStudent
+// }
